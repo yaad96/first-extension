b2b9914fb920b0e4d3a6b2630b92b97e07f48446
mainulyaad <yaadmainul@gmail.com>
2024-03-02 20:48:29 -0500

    just testing

--- src/Constants.ts
+++ src/Constants.ts
@@ -7,6 +7,7 @@ export const Constants = {
     LEARNING_DR_DIRECTORY:"/LearningDR",
     SRCML_PATH_WINDOWS: "C:\\Program Files\\srcML 0.9.5\\bin\\srcml",
     SRCML_PATH_MAC: "/usr/local/bin/srcml",
-    SRCML_PATH_LINUX: "/usr/bin/srcml"
+    SRCML_PATH_LINUX: "/usr/bin/srcml",
+    JUST_FOR_THE_KICKS:"hello"
 
 };
